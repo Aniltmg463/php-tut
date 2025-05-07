eaa76ed36f8642662f740723431a848026d9223b
https://github.com/Aniltmg463/Tech-hub.git <aniltamang462@gmail.com>
2025-05-08 05:16:10 +0545

    simple-crud operation done

---
+++ delete.php
@@ -0,0 +1,14 @@
+<?php
+include 'connect.php';
+
+if (isset($_GET['deleteid'])) {
+    $id = $_GET['deleteid'];
+    $sql = "DELETE FROM `students` WHERE id=$id";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        //echo "Data deleted successfully";
+        header('location:display.php');
+    } else {
+        die(mysqli_error($conn));
+    }
+}
