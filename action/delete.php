ac1d2e7a07942727556b4e5f2e0cb2bbbd382222
https://github.com/Aniltmg463/Tech-hub.git <aniltamang462@gmail.com>
2025-05-16 06:45:10 +0545

    adding  alert msg notfification in delete.php

--- action/delete.php
+++ action/delete.php
@@ -7,6 +7,8 @@ if (isset($_GET['deleteid'])) {
     $result = mysqli_query($conn, $sql);
     if ($result) {
         //echo "Data deleted successfully";
+        $_SESSION['message'] = "Student deleted successfully.";
+        $_SESSION['message_type'] = "success";
         header('location:../index.php');
     } else {
         die(mysqli_error($conn));
