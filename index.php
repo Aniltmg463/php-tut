bbd07f55c3761149c27bf6dfa1ffea520115b845
Anil463 <aniltamang462@gmail.com>
2025-05-28 09:13:59 +0545

    Update index.php

--- index.php
+++ index.php
@@ -2,30 +2,51 @@
 require_once 'config/Database.php';
 require_once 'classes/Job.php';
 
-$db = (new Database())->connect();
-$job = new Job($db);
+$db = new Database();
+$conn = $db->connect();
+// $db = (new Database())->connect(); //in single line
+$job = new Job($conn);
+
+/* echo '<pre>';
+print_r($job);
+echo '</pre>';
+die;
+ */
 
 $action = $_GET['action'] ?? '';
+$method = $_SERVER['REQUEST_METHOD'];
 
-if ($action === 'create' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-    $job->create($_POST['title'], $_POST['company'], $_POST['location']);
-    header("Location: index.php");
-    exit;
-} elseif ($action === 'create') {
-    include 'views/create.php';
-} elseif ($action === 'edit' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-    $job->update($_GET['id'], $_POST['title'], $_POST['company'], $_POST['location']);
-    header("Location: index.php");
-    exit;
-} elseif ($action === 'edit') {
-    $jobData = $job->getOne($_GET['id']);
-    $job = $jobData;
-    include 'views/edit.php';
-} elseif ($action === 'delete') {
-    $job->delete($_GET['id']);
-    header("Location: index.php");
-    exit;
-} else {
-    $jobs = $job->getAll();
-    include 'views/index.php';
+switch ($action) {
+    case 'create':
+        if ($method === 'POST') {
+            $job->create($_POST['title'], $_POST['company'], $_POST['location']);
+            header("Location: index.php");
+            exit;
+        } else {
+            include 'views/create.php';
+        }
+        break;
+
+    case 'edit':
+        if ($method === 'POST') {
+            $job->update($_GET['id'], $_POST['title'], $_POST['company'], $_POST['location']);
+            header("Location: index.php");
+            exit;
+        } else {
+            $jobData = $job->getOne($_GET['id']);
+            $job = $jobData;
+            include 'views/edit.php';
+        }
+        break;
+
+    case 'delete':
+        $job->delete($_GET['id']);
+        header("Location: index.php");
+        exit;
+        break;
+
+    default:
+        $jobs = $job->getAll();
+        include 'views/index.php';
+        break;
 }
